b70dcf57df313835889d2b96f50cf7d459ed677e
Simon Moore <simon.moore@cl.cam.ac.uk>
2019-03-20 18:07:46 +0000

    Correct the fib() function.

--- fib/src/fib.c
+++ fib/src/fib.c
@@ -1,12 +1,16 @@
-int fib(const unsigned int a)
+int fib(const int a)
 {
   if(a<2)
-    return 1;
+    return a;
   else
     return fib(a-1) + fib(a-2);
 }
 
-void main(void)
+int main(void)
 {
-  fib(10);
+  int *outputDevice = (int*) 0xf0000000; // magic output device
+  int result;
+  result = fib(8);
+  outputDevice[0] = result;
+  return result;
 }
