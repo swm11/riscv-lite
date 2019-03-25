862cd79764fc2c31518be5cf5877240e023d464a
Andrew Rice <acr31@cam.ac.uk>
2019-03-25 17:27:28 +0000

    Applied a java source code layout style

--- jsim/src/uk/ac/cam/swm11/riscvlite/memory.java
+++ jsim/src/uk/ac/cam/swm11/riscvlite/memory.java
@@ -1,84 +1,75 @@
 package uk.ac.cam.swm11.riscvlite;
 
-import java.nio.file.FileSystems;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.IOException;
 
-class memory
-{
-    private int mem[];
+class memory {
+  private int mem[];
 
-    // initialise a memory of a given size with a binary image from a file
-    public void memory(int memsizebytes, String filepath) throws IOException
-    {
-	// initialise the memory
-	int memsizewords = memsizebytes/4;
-	this.mem = new int [memsizewords];
-	for(int a=0; a<memsizewords; a++) {
-	    this.mem[a] = 0;
-	}
-	System.out.format("Memory size = %d words = %d bytes = %d KiB\n",
-			  this.mem.length, this.mem.length*4, this.mem.length*4/1024);
-	// load binary image into the memory
-        byte [] b = Files.readAllBytes(Paths.get(filepath));
-	// copy bytes from byte buffer into our word-sized mem[] array
-	for(int a=0; (a<this.mem.length) && ((a*4+3)<b.length); a++) {
-	    this.mem[a] = ((b[a*4+3] & 0xff)<<24)
-		        | ((b[a*4+2] & 0xff)<<16)
-    		        | ((b[a*4+1] & 0xff)<< 8)
-		        | ((b[a*4+0] & 0xff));
-	}
-    } 
-
-    private boolean checkValidAddress(int byteaddress)
-    {
-	boolean valid;                            // check:
-	valid = ((byteaddress & 0x3)==0)          //  alignment requirement
-	     && ((byteaddress/4)<this.mem.length) //  upper bound
-	     && (byteaddress>=0);                 //  lower bound
-	if(!valid) {
-	    if((byteaddress & 0x3)==0) {
-		System.out.format("ERROR: out of range address sent to memory: 0x%08x = %d\n",
-				  byteaddress, byteaddress);
-	    } else {
-		System.out.format("ERROR: incorrectly aligned address sent to memory: 0x%08x = 0b%s\n",
-				  byteaddress,
-				  String.format("%7s", Integer.toBinaryString(byteaddress)).replace(' ', '0')); // nasty hack to get exaclty 32 binary digits
-	    }
-	}
-	return valid;
+  // initialise a memory of a given size with a binary image from a file
+  public void memory(int memsizebytes, String filepath) throws IOException {
+    // initialise the memory
+    int memsizewords = memsizebytes / 4;
+    this.mem = new int[memsizewords];
+    for (int a = 0; a < memsizewords; a++) {
+      this.mem[a] = 0;
     }
-    
-    public int load(int byteaddress)
-    {
-        if(this.checkValidAddress(byteaddress))
-	    return this.mem[byteaddress/4];
-	else
-	    return 0xdead0000;
+    System.out.format(
+        "Memory size = %d words = %d bytes = %d KiB\n",
+        this.mem.length, this.mem.length * 4, this.mem.length * 4 / 1024);
+    // load binary image into the memory
+    byte[] b = Files.readAllBytes(Paths.get(filepath));
+    // copy bytes from byte buffer into our word-sized mem[] array
+    for (int a = 0; (a < this.mem.length) && ((a * 4 + 3) < b.length); a++) {
+      this.mem[a] =
+          ((b[a * 4 + 3] & 0xff) << 24)
+              | ((b[a * 4 + 2] & 0xff) << 16)
+              | ((b[a * 4 + 1] & 0xff) << 8)
+              | ((b[a * 4 + 0] & 0xff));
     }
+  }
 
-    public void store(int byteaddress, int data)
-    {
-	if(byteaddress == 0xf0000000) {// magic output device detected
-	    System.out.format("**********************************************************************\n");
-	    System.out.format("* RESULT = 0x%08x = %d\n",data,data);
-	    System.out.format("**********************************************************************\n");
-	} else if(this.checkValidAddress(byteaddress))
-	    this.mem[byteaddress/4] = data;
+  private boolean checkValidAddress(int byteaddress) {
+    boolean valid; // check:
+    valid =
+        ((byteaddress & 0x3) == 0) //  alignment requirement
+            && ((byteaddress / 4) < this.mem.length) //  upper bound
+            && (byteaddress >= 0); //  lower bound
+    if (!valid) {
+      if ((byteaddress & 0x3) == 0) {
+        System.out.format(
+            "ERROR: out of range address sent to memory: 0x%08x = %d\n", byteaddress, byteaddress);
+      } else {
+        System.out.format(
+            "ERROR: incorrectly aligned address sent to memory: 0x%08x = 0b%s\n",
+            byteaddress,
+            String.format("%7s", Integer.toBinaryString(byteaddress))
+                .replace(' ', '0')); // nasty hack to get exaclty 32 binary digits
+      }
     }
+    return valid;
+  }
 
-    public void hexdump(int to, int from)
-    {
-	from = from/4;
-	to = to/4;
-	if(from<0)
-	    from = 0;
-	if(to>=mem.length)
-	    to = mem.length-1;
-	for(int a=to; a<=from; a++)
-	    System.out.format("mem[0x%08x] = 0x%08x = %d\n",
-			      a*4, this.mem[a], this.mem[a]);
-    }
-    
+  public int load(int byteaddress) {
+    if (this.checkValidAddress(byteaddress)) return this.mem[byteaddress / 4];
+    else return 0xdead0000;
+  }
+
+  public void store(int byteaddress, int data) {
+    if (byteaddress == 0xf0000000) { // magic output device detected
+      System.out.format("**********************************************************************\n");
+      System.out.format("* RESULT = 0x%08x = %d\n", data, data);
+      System.out.format("**********************************************************************\n");
+    } else if (this.checkValidAddress(byteaddress)) this.mem[byteaddress / 4] = data;
+  }
+
+  public void hexdump(int to, int from) {
+    from = from / 4;
+    to = to / 4;
+    if (from < 0) from = 0;
+    if (to >= mem.length) to = mem.length - 1;
+    for (int a = to; a <= from; a++)
+      System.out.format("mem[0x%08x] = 0x%08x = %d\n", a * 4, this.mem[a], this.mem[a]);
+  }
 }
--- jsim/src/uk/ac/cam/swm11/riscvlite/processor.java
+++ jsim/src/uk/ac/cam/swm11/riscvlite/processor.java
@@ -2,247 +2,276 @@ package uk.ac.cam.swm11.riscvlite;
 
 import java.io.IOException;
 
-class processor
-{
-    public enum executeState {STOPPED, RUNNING};
-    
-    private final String regABInameStr[] = {
-      "zero", "ra",   "sp",   "gp",
-      "tp",   "t0",   "t1",   "t2",
-      "fp",   "s1",   "a0",   "a1",
-      "a2",   "a3",   "a4",   "a5",
-      "a6",   "a7",   "s2",   "s3",
-      "s4",   "s5",   "s6",   "s7",
-      "s8",   "s9",   "s10",  "s11",
-      "t3",   "t4",   "t5",   "t6" };
+class processor {
+  public enum executeState {
+    STOPPED,
+    RUNNING
+  };
 
-    private enum instClass {Rtype, Itype, Stype, Btype, Utype, Jtype, Udef};
-    
-    private enum inst_t {UDEF, ADD, ADDI, AUIPC, BLT, J, JAL, JALR, LW, LUI, MV, SW};
+  private final String regABInameStr[] = {
+    "zero", "ra", "sp", "gp",
+    "tp", "t0", "t1", "t2",
+    "fp", "s1", "a0", "a1",
+    "a2", "a3", "a4", "a5",
+    "a6", "a7", "s2", "s3",
+    "s4", "s5", "s6", "s7",
+    "s8", "s9", "s10", "s11",
+    "t3", "t4", "t5", "t6"
+  };
 
-    // Decoded instruction type
-    private class decodedInst {
-	public instClass typ;  // decoded instruction type
-	public inst_t    inst; // decoded instruction
-	public int       imm;  // decoded immediate operand
-	public byte      rd, rs1, rs2; // registers
-	public byte      opcode, funct3, funct7;
+  private enum instClass {
+    Rtype,
+    Itype,
+    Stype,
+    Btype,
+    Utype,
+    Jtype,
+    Udef
+  };
+
+  private enum inst_t {
+    UDEF,
+    ADD,
+    ADDI,
+    AUIPC,
+    BLT,
+    J,
+    JAL,
+    JALR,
+    LW,
+    LUI,
+    MV,
+    SW
+  };
+
+  // Decoded instruction type
+  private class decodedInst {
+    public instClass typ; // decoded instruction type
+    public inst_t inst; // decoded instruction
+    public int imm; // decoded immediate operand
+    public byte rd, rs1, rs2; // registers
+    public byte opcode, funct3, funct7;
+  }
+
+  // Architectural state of the processor
+  private class archState {
+    int pc, nextpc; // current and next program counter values
+    int rf[]; // register file
+  }
+
+  // Storage for the memory and architectural state
+  private memory mem;
+  private archState archst;
+
+  /** Initialise memory and architectural state. */
+  public void processor(int memsizebytes, String programfilepath, int startPC) throws IOException {
+    this.mem = new memory();
+    this.mem.memory(memsizebytes, programfilepath);
+    archst = new archState();
+    archst.nextpc = startPC;
+    archst.pc = -1; // value should never be looked at, so set to an invalid value
+    archst.rf = new int[32];
+    for (int r = 0; r < 32; r++) {
+      archst.rf[r] = 0;
+    }
+  }
+
+  /** Decode an instruction. */
+  private decodedInst decode(int inst) {
+    decodedInst d = new decodedInst();
+    // Decode the fixed fields even if they are not needed for a particular instruction
+    d.opcode = bitextract.bitExtractByte(inst, 0, 6);
+    d.rd = bitextract.bitExtractByte(inst, 7, 11);
+    d.rs1 = bitextract.bitExtractByte(inst, 15, 19);
+    d.rs2 = bitextract.bitExtractByte(inst, 20, 24);
+    d.funct3 = bitextract.bitExtractByte(inst, 12, 14);
+    d.funct7 = bitextract.bitExtractByte(inst, 25, 31);
+    switch (d.opcode) {
+      case 0b0110011: // R-type instructions (e.g. ADD)
+        d.typ = instClass.Rtype;
+        d.imm = 0;
+        switch ((d.funct7 << 3) | d.funct3) {
+          case ((0b000000 << 3) | 0b000):
+            d.inst = inst_t.ADD;
+            break;
+          default:
+            d.inst = inst_t.UDEF;
+        }
+        break;
+
+      case 0b0010011: // I-type instructions (e.g. ADDI)
+      case 0b0000011: // also load instructions
+      case 0b1100111: // also JALR
+        d.typ = instClass.Itype;
+        d.imm = bitextract.bitExtractSignedInt(inst, 20, 31);
+        switch ((d.opcode << 3) | d.funct3) {
+          case (0b0010011 << 3) | 0b000:
+            d.inst = inst_t.ADDI;
+            break;
+          case (0b0000011 << 3) | 0b010:
+            d.inst = inst_t.LW;
+            break;
+          case (0b1100111 << 3) | 0b000:
+            d.inst = inst_t.JALR;
+            break;
+          default:
+            d.inst = inst_t.UDEF;
+        }
+        break;
+      case 0b0100011: // S-type (store instructions)
+        d.typ = instClass.Stype;
+        d.imm = (bitextract.bitExtractSignedInt(inst, 25, 31) << 5) | d.rd;
+        switch (d.funct3) {
+          case 0b010:
+            d.inst = inst_t.SW;
+            break;
+          default:
+            d.inst = inst_t.UDEF;
+        }
+        break;
+      case 0b0010111: // AUIPC
+        d.typ = instClass.Utype;
+        d.imm = bitextract.bitExtractInt(inst, 12, 31) << 12;
+        d.inst = inst_t.AUIPC;
+        break;
+      case 0b0110111: // LUI
+        d.typ = instClass.Utype;
+        d.imm = bitextract.bitExtractInt(inst, 12, 31) << 12;
+        d.inst = inst_t.LUI;
+        break;
+      case 0b1101111: // JAL (J-type)
+        d.typ = instClass.Jtype;
+        d.imm =
+            (bitextract.bitExtractInt(inst, 21, 30) << 1)
+                | (bitextract.bitExtractInt(inst, 20, 20) << 11)
+                | (bitextract.bitExtractInt(inst, 12, 19) << 12)
+                | (bitextract.bitExtractSignedInt(inst, 31, 31) << 20);
+        d.inst = inst_t.JAL;
+        break;
+        //  B-type instrucitons
+      case 0b1100011: // conditional branches
+        d.typ = instClass.Btype;
+        d.imm =
+            bitextract.bitExtractInt(inst, 8, 11) << 1
+                | bitextract.bitExtractInt(inst, 25, 30) << 5
+                | bitextract.bitExtractInt(inst, 7, 7) << 11
+                | bitextract.bitExtractSignedInt(inst, 31, 31) << 12;
+        switch (d.funct3) {
+          case 0b100:
+            d.inst = inst_t.BLT;
+            break;
+          default:
+            d.inst = inst_t.UDEF;
+        }
+        break;
+
+      default:
+        d.typ = instClass.Udef;
+        d.inst = inst_t.UDEF;
+        d.imm = 0;
     }
 
-    // Architectural state of the processor
-    private class archState {
-	int pc, nextpc;  // current and next program counter values
-	int rf[];        // register file
+    return d;
+  }
+
+  public executeState executeStep() {
+    decodedInst d;
+    // Move onto the next pc
+    archst.pc = archst.nextpc;
+    // Ensure register zero is always 0
+    archst.rf[0] = 0;
+    // Fetch and decode instruction
+    d = decode(this.mem.load(archst.pc));
+    // By default the nextpc is the next instruction
+    archst.nextpc = archst.pc + 4;
+    switch (d.inst) {
+      case ADD: // add two registers
+        archst.rf[d.rd] = archst.rf[d.rs1] + archst.rf[d.rs2];
+        break;
+      case ADDI: // add a register and an immediate (i.e. a constant)
+        archst.rf[d.rd] = archst.rf[d.rs1] + d.imm;
+        break;
+      case AUIPC: // program counter relative immediate
+        archst.rf[d.rd] = archst.pc + d.imm;
+        break;
+      case LUI: // load upper immediate
+        archst.rf[d.rd] = d.imm;
+        break;
+      case BLT: // branch less than
+        if (archst.rf[d.rs1] < archst.rf[d.rs2]) archst.nextpc = archst.pc + d.imm;
+        break;
+      case JAL: // jump and link (pc + immediate)
+        archst.nextpc = archst.pc + d.imm;
+        archst.rf[1] = archst.pc + 4; // x1 = ra (return address)
+        break;
+      case JALR: // jump and link (register + immediate)
+        archst.nextpc = archst.rf[d.rs1] + d.imm;
+        archst.rf[1] = archst.pc + 4; // x1 = ra (return address)
+        break;
+      case LW: // load word
+        archst.rf[d.rd] = this.mem.load(archst.rf[d.rs1] + d.imm);
+        break;
+      case SW: // store word
+        this.mem.store(archst.rf[d.rs1] + d.imm, archst.rf[d.rs2]);
+        break;
+      default:
+        System.out.format("ERROR: Undefined instruction at pc=0x%08x\n", archst.pc);
+        archst.nextpc = archst.pc; // trigger stop condition
     }
 
-    // Storage for the memory and architectural state
-    private memory mem;
-    private archState archst;
+    return archst.nextpc == archst.pc ? executeState.STOPPED : executeState.RUNNING;
+  }
 
-    /**
-     * Initialise memory and architectural state.
-     */
-    public void processor(
-			  int memsizebytes,
-			  String programfilepath,
-			  int startPC
-			  ) throws IOException
-    {
-	this.mem = new memory();
-	this.mem.memory(memsizebytes, programfilepath);
-	archst = new archState();
-	archst.nextpc = startPC;
-	archst.pc = -1; // value should never be looked at, so set to an invalid value
-	archst.rf = new int[32];
-	for(int r=0; r<32; r++) {
-	    archst.rf[r] = 0;
-	}
+  /** Dump memory with instructions decoded. */
+  public void decodedump(memory mem, int lowerBound, int upperBound) {
+    int a, m;
+    for (a = lowerBound; a <= upperBound; a = a + 4) {
+      m = this.mem.load(a);
+      decodedInst d = decode(m);
+      System.out.format(
+          "0x%04x: 0x%08x opcode=%s typ=%-5s inst=%-5s rd=%-4s rs1=%-4s rs2=%-4s imm=0x%08x=%d\n",
+          a,
+          m,
+          String.format("%7s", Integer.toBinaryString(d.opcode))
+              .replace(' ', '0'), // nasty hack to get exaclty 7 binary digits
+          d.typ.name(),
+          d.inst.name(),
+          regABInameStr[d.rd],
+          regABInameStr[d.rs1],
+          regABInameStr[d.rs2],
+          d.imm,
+          d.imm);
     }
-    
-    /**
-     * Decode an instruction.
-     */
-    private decodedInst decode(int inst)
-    {
-	decodedInst d = new decodedInst();
-	// Decode the fixed fields even if they are not needed for a particular instruction
-	d.opcode = bitextract.bitExtractByte(inst,0,6);
-	d.rd = bitextract.bitExtractByte(inst,7,11);
-	d.rs1 = bitextract.bitExtractByte(inst,15,19);
-	d.rs2 = bitextract.bitExtractByte(inst,20,24);
-	d.funct3 = bitextract.bitExtractByte(inst,12,14);
-	d.funct7 = bitextract.bitExtractByte(inst,25,31);
-	switch(d.opcode) {
-	case 0b0110011: // R-type instructions (e.g. ADD)
-	    d.typ = instClass.Rtype;
-	    d.imm = 0;
-	    switch((d.funct7<<3) | d.funct3) {
-	    case ((0b000000<<3) | 0b000): d.inst = inst_t.ADD; break;
-	    default: d.inst = inst_t.UDEF;
-	    }
-	    break;
-                  
-	case 0b0010011: // I-type instructions (e.g. ADDI)
-	case 0b0000011: // also load instructions
-	case 0b1100111: // also JALR
-	    d.typ = instClass.Itype;
-	    d.imm = bitextract.bitExtractSignedInt(inst,20,31);
-	    switch((d.opcode<<3) | d.funct3) {
-	    case (0b0010011<<3) | 0b000: d.inst = inst_t.ADDI; break;
-	    case (0b0000011<<3) | 0b010: d.inst = inst_t.LW;   break;
-	    case (0b1100111<<3) | 0b000: d.inst = inst_t.JALR; break;
-	    default: d.inst = inst_t.UDEF;
-	    }
-	    break;
-	case 0b0100011: // S-type (store instructions)
-	    d.typ = instClass.Stype;
-	    d.imm = (bitextract.bitExtractSignedInt(inst,25,31)<<5) | d.rd;
-	    switch(d.funct3) {
-	    case 0b010: d.inst = inst_t.SW; break;
-	    default: d.inst = inst_t.UDEF;
-	    }
-	    break;
-	case 0b0010111: // AUIPC
-	    d.typ = instClass.Utype;
-	    d.imm = bitextract.bitExtractInt(inst,12,31)<<12;
-	    d.inst = inst_t.AUIPC;
-	    break;
-	case 0b0110111: // LUI
-	    d.typ = instClass.Utype;
-	    d.imm = bitextract.bitExtractInt(inst,12,31)<<12;
-	    d.inst = inst_t.LUI;
-	    break;
-	case 0b1101111: // JAL (J-type)
-	    d.typ = instClass.Jtype;
-	    d.imm =
-		(bitextract.bitExtractInt(inst,21,30)<< 1) |
-		(bitextract.bitExtractInt(inst,20,20)<<11) |
-		(bitextract.bitExtractInt(inst,12,19)<<12) |
-		(bitextract.bitExtractSignedInt(inst,31,31)<<20);
-	    d.inst = inst_t.JAL;
-	    break;
-	                //  B-type instrucitons
-	case 0b1100011: // conditional branches
-	    d.typ = instClass.Btype;
-	    d.imm = bitextract.bitExtractInt(inst,8,11)<<1 |
-		bitextract.bitExtractInt(inst,25,30)<<5 |
-		bitextract.bitExtractInt(inst,7,7)<<11 |
-		bitextract.bitExtractSignedInt(inst,31,31)<<12;
-	    switch(d.funct3) {
-	    case 0b100: d.inst = inst_t.BLT; break;
-	    default: d.inst = inst_t.UDEF;
-	    }
-	    break;
-	    
-	default:
-	    d.typ = instClass.Udef;
-	    d.inst = inst_t.UDEF;
-	    d.imm = 0;
-	}
+  }
 
-	return d;
+  /** Report on instruction executed. */
+  public void traceExecutedInstruction() {
+    decodedInst d;
+    d = decode(this.mem.load(archst.pc)); // fetch and decode instruction
+    System.out.format(
+        "pc=0x%08x inst=%5s rd=x%02d=%4s=%-8d rs1=%4s=%-8d rs2=%4s=%-8d imm=0x%08x=%d\n",
+        archst.pc,
+        d.inst.name(),
+        d.rd,
+        regABInameStr[d.rd],
+        archst.rf[d.rd],
+        regABInameStr[d.rs1],
+        archst.rf[d.rs1],
+        regABInameStr[d.rs2],
+        archst.rf[d.rs2],
+        d.imm,
+        d.imm);
+    if (archst.nextpc != archst.pc + 4) {
+      System.out.format(
+          "--------------------jump: 0x%08x->0x%08x --------------------\n",
+          archst.pc, archst.nextpc);
     }
-
-
-    public executeState executeStep()
-    {
-	decodedInst d;
-	// Move onto the next pc	
-	archst.pc = archst.nextpc;
-	// Ensure register zero is always 0
-	archst.rf[0] = 0;
-	// Fetch and decode instruction
-	d = decode(this.mem.load(archst.pc)); 
-	// By default the nextpc is the next instruction
-	archst.nextpc = archst.pc+4;
-	switch(d.inst) {
-	case ADD:    // add two registers
-	    archst.rf[d.rd] = archst.rf[d.rs1] + archst.rf[d.rs2];
-	    break;
-	case ADDI:   // add a register and an immediate (i.e. a constant)
-	    archst.rf[d.rd] = archst.rf[d.rs1] + d.imm;
-	    break;
-	case AUIPC:  // program counter relative immediate
-	    archst.rf[d.rd] = archst.pc + d.imm;
-	    break;
-	case LUI:    // load upper immediate
-	    archst.rf[d.rd] = d.imm;
-	    break;
-	case BLT:    // branch less than
-	    if(archst.rf[d.rs1] < archst.rf[d.rs2])
-		archst.nextpc = archst.pc + d.imm;
-	    break;
-	case JAL:    // jump and link (pc + immediate)
-	    archst.nextpc = archst.pc + d.imm;
-	    archst.rf[1] = archst.pc+4; // x1 = ra (return address)
-	    break;
-	case JALR:   // jump and link (register + immediate)
-	    archst.nextpc = archst.rf[d.rs1] + d.imm;
-	    archst.rf[1] = archst.pc+4; // x1 = ra (return address)
-	    break;
-	case LW:     // load word
-	    archst.rf[d.rd] = this.mem.load(archst.rf[d.rs1] + d.imm);
-	    break;
-	case SW:     // store word
-	    this.mem.store(archst.rf[d.rs1] + d.imm, archst.rf[d.rs2]);
-	    break;
-	default:
-	    System.out.format("ERROR: Undefined instruction at pc=0x%08x\n",archst.pc);
-	    archst.nextpc=archst.pc; // trigger stop condition
-	}
-
-	return archst.nextpc==archst.pc ? executeState.STOPPED
-	                                : executeState.RUNNING;
+    if (archst.nextpc == archst.pc) {
+      System.out.format("--------------------STOPPED--------------------\nRegister map:\n");
+      for (int r = 0; r < 32; r++) {
+        System.out.format(
+            "  x%02d = %4s = 0x%08x = %d\n", r, regABInameStr[r], archst.rf[r], archst.rf[r]);
+      }
     }
-
-    
-    /**
-     * Dump memory with instructions decoded.
-     */
-    public void decodedump(
-			   memory mem,
-			   int lowerBound,
-			   int upperBound)
-    {
-	int a, m;
-	for(a=lowerBound; a<=upperBound; a=a+4) {
-	    m = this.mem.load(a);
-	    decodedInst d = decode(m);
-	    System.out.format(
-	        "0x%04x: 0x%08x opcode=%s typ=%-5s inst=%-5s rd=%-4s rs1=%-4s rs2=%-4s imm=0x%08x=%d\n",
-		a, m,
-		String.format("%7s", Integer.toBinaryString(d.opcode)).replace(' ', '0'), // nasty hack to get exaclty 7 binary digits
-		d.typ.name(), d.inst.name(),
-		regABInameStr[d.rd],
-		regABInameStr[d.rs1], regABInameStr[d.rs2],
-		d.imm, d.imm);
-	}
-    }
-
-    /**
-     * Report on instruction executed.
-     */
-    public void traceExecutedInstruction()
-    {
-	decodedInst d;
-	d = decode(this.mem.load(archst.pc)); // fetch and decode instruction
-	System.out.format(
-            "pc=0x%08x inst=%5s rd=x%02d=%4s=%-8d rs1=%4s=%-8d rs2=%4s=%-8d imm=0x%08x=%d\n",
-	    archst.pc,
-	    d.inst.name(),
-	    d.rd, regABInameStr[d.rd], archst.rf[d.rd], 
-	    regABInameStr[d.rs1], archst.rf[d.rs1],
-	    regABInameStr[d.rs2], archst.rf[d.rs2],
-	    d.imm, d.imm);
-	if(archst.nextpc != archst.pc+4) {
-	    System.out.format("--------------------jump: 0x%08x->0x%08x --------------------\n", archst.pc, archst.nextpc);
-	}
-	if(archst.nextpc==archst.pc) {
-	    System.out.format("--------------------STOPPED--------------------\nRegister map:\n");
-	    for(int r=0; r<32; r++) {
-		System.out.format("  x%02d = %4s = 0x%08x = %d\n",
-				  r, regABInameStr[r], archst.rf[r], archst.rf[r]);
-	    }
-	}
-    }
+  }
 }
