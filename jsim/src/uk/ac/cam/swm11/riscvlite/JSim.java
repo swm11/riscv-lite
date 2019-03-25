251ffb9708fdefcae4a8e224e287ea8559f2f5be
Andrew Rice <acr31@cam.ac.uk>
2019-03-25 19:34:25 +0000

    threw exception from Main rather than printing error message

--- jsim/src/uk/ac/cam/swm11/riscvlite/JSim.java
+++ jsim/src/uk/ac/cam/swm11/riscvlite/JSim.java
@@ -3,19 +3,13 @@ package uk.ac.cam.swm11.riscvlite;
 import java.io.IOException;
 
 class JSim {
-  public static void main(String[] args) {
-    ArchState.ExecuteState ps;
-    Processor proc;
-    try {
-      // initialise processor and load program binary
-      proc = Processor.initialize(65 * 1024, "../fib/build/mem.bin", 0);
-    } catch (IOException e) {
-      System.out.format("ERROR: Failed to read binary initialisation file.%n");
-      return;
-    }
+  public static void main(String[] args) throws IOException {
+    // initialise processor and load program binary
+    Processor proc = Processor.initialize(65 * 1024, "../fib/build/mem.bin", 0);
     System.out.format("Decoded dump of the initial memory:%n");
     proc.decodeDump(0, 50 * 4);
     // step through execution until the stop condition is met
+    ArchState.ExecuteState ps;
     do {
       ps = proc.executeStep();
       proc.traceExecutedInstruction();
